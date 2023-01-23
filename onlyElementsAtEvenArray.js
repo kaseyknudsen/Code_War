29df520ae9901a149d02cb83d6815efdf6ad5623
Kasey Knudsen <69942382+kaseyknudsen@users.noreply.github.com>
2023-01-23 12:41:31 -0800

    Add files via upload
    A function the prints the elements at the even indices of an array

---
+++ onlyElementsAtEvenArray.js
@@ -0,0 +1,11 @@
+function onlyElementsAtEvenIndex(array) {
+  const newArray = Array(Math.ceil(array.length / 2));
+  console.log(newArray);
+  for (var i = 0; i < array.length; i++) {
+    if (i % 2 === 0) {
+      newArray[i / 2] = array[i];
+    }
+  }
+  return newArray;
+}
+console.log(onlyElementsAtEvenIndex([1, 2, 3, 4, 5, 6, 7]));
